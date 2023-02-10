2392668ef5a5773c353da60d147009bc39f57f66
Diksha4248 <88928334+Diksha4248@users.noreply.github.com>
2023-02-10 15:07:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -136,5 +136,7 @@ def fear():
 
 
 if __name__=="__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)                   
+    app.run(debug=True)
+#     from waitress import serve
+#     serve(app, host="0.0.0.0", port=8080)  
+
