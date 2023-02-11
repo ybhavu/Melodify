685e9bbe3697d8798a1dcdbcf1b77c92a22d4082
Vaibhav Upganlawar <79738677+ybhavu@users.noreply.github.com>
2023-02-11 14:18:38 +0530

    m

--- app.py
+++ app.py
@@ -17,49 +17,10 @@ app=Flask(__name__)
 camera=cv2.VideoCapture(0)
 
 
-final_emotion = ""
-def recog(frame):
-    i=0
-    output=[]
-    while (i<=0):
-        labels = []
-        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-        faces = face_classifier.detectMultiScale(gray)
-
-        for (x,y,w,h) in faces:
-            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,255),2)
-            roi_gray = gray[y:y+h,x:x+w]
-            roi_gray = cv2.resize(roi_gray,(48,48),interpolation=cv2.INTER_AREA)
-
-
-
-            if np.sum([roi_gray])!=0:
-                roi = roi_gray.astype('float')/255.0
-                roi = img_to_array(roi)
-                roi = np.expand_dims(roi,axis=0)
-
-                prediction = classifier.predict(roi)[0]
-                label=emotion_labels[prediction.argmax()]
-                label_position = (x,y)
-                max_index = np.argmax(label)
-                output.append(label)
-                cv2.putText(frame,label,label_position,cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
-            else:
-                cv2.putText(frame,'No Faces',(30,80),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
-        i = i+1        
-        key = cv2.waitKey(1)
-        if key == 12: 
-                camera.release()
-                cv2.destroyAllWindows()
-                break
-
-    arr = np.array(output)
-    global final_emotion
-    final_emotion = st.mode(arr)
-        
-
+final_emotion=""
 def generate_frames():
     j=0
+    output=[]
     while (j<=30):
             
         ## read the camera frame
@@ -67,17 +28,53 @@ def generate_frames():
         if not success:
             break
         else:
+            if j%1 == 0:
 
-            recog(frame)
-            ret,buffer=cv2.imencode('.jpg',frame)
-            frame=buffer.tobytes()
+                labels = []
+                gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+                faces = face_classifier.detectMultiScale(gray)
+
+                for (x,y,w,h) in faces:
+                    cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,255),2)
+                    roi_gray = gray[y:y+h,x:x+w]
+                    roi_gray = cv2.resize(roi_gray,(48,48),interpolation=cv2.INTER_AREA)
 
 
+
+                    if np.sum([roi_gray])!=0:
+                        roi = roi_gray.astype('float')/255.0
+                        roi = img_to_array(roi)
+                        roi = np.expand_dims(roi,axis=0)
+
+                        prediction = classifier.predict(roi)[0]
+                        label=emotion_labels[prediction.argmax()]
+                        label_position = (x,y)
+                        max_index = np.argmax(label)
+                        output.append(label)
+                        cv2.putText(frame,label,label_position,cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+                    else:
+                        cv2.putText(frame,'No Faces',(30,80),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)       
+                key = cv2.waitKey(1)
+                if key == 12: 
+                        camera.release()
+                        cv2.destroyAllWindows()
+                        break
+
+                
+
+                
+        ret,buffer=cv2.imencode('.jpg',frame)
+        frame=buffer.tobytes()
         j=j+1
         yield(b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        
+    arr = np.array(output)
+    global final_emotion
+    final_emotion = st.mode(arr)    
 
-    print("predicted emotion = ",final_emotion)  
+    print("predicted emotion = ",final_emotion)
+    camera.release()  
 
     return render_template('emotion.html')      
 
@@ -136,4 +133,5 @@ def fear():
 
 
 if __name__=="__main__":
-    app.run(debug=True)                   
+    app.run(debug=True)
+   
